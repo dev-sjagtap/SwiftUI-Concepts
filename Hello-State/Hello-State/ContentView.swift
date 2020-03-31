c7bcb327b037562c548c0b14cb90182140291e6a
Sushant Jagtap <sushantjagtap9945@xento.com>
2020-03-31 10:27:00 +0530

    # Task Name:  Understand state in swiftUI - Just started with state in swiftUI.

---
+++ Hello-State/Hello-State/ContentView.swift
@@ -0,0 +1,25 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var name: String = "John"
+    
+    var body: some View {
+        VStack {
+            Text(name)
+                .font(.largeTitle)
+            Button(action: {
+                self.name = "Mary"
+            }) {
+                Text("Change Name")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
